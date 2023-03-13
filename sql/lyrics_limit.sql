29d37e249ca11c8bc49fafc7db4c134fe136a9a5
Jakobus Möhring <jmoehring@live.de>
2023-03-13 09:57:33 +0100

    feat: add notebooks and queries

--- sql/lyrics_limit.sql
+++ sql/lyrics_limit.sql
@@ -1,3 +1,3 @@
 SELECT * FROM lyrics WHERE track_id IN (
-    SELECT track_id FROM lyrics GROUP BY track_id LIMIT 1000
+    SELECT track_id FROM lyrics GROUP BY track_id ORDER BY random() LIMIT 1000
 )
